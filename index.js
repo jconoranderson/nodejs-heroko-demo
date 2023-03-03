e9746c0095daedb3f49df0f8ebfd1a1bc3be370f
Conor Anderson <canderson@centers-mbp.mynetworksettings.com>
2023-03-02 22:49:02 -0500

    test

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000
     and replace "mongoAtlasUri" with your mongodb atlas uri.
 */
 // mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js" 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,3 +16,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
+
+$.ajax({
+    url: "https://health.data.ny.gov/resource/nvtm-yit2.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "aGFbKDOg0gCeCm2c9Od2EsFjL"
+    }
+}).done(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+});
